7d7408b1ccc834e2ae1004e33a26bfd597d096ad
BaHaDuR299 <tu7ad7gex@code.edu.az>
2022-03-05 11:47:10 +0300

    visual 1.1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           {
+
+
+            
+                //////String[] array = new string[] { "sunal","javid","gulnar","bahadur","baxseli" };
+                //////for (int i = 0; i < array.Length; i++) 
+                //////{
+                //////    if (array[i].Length > 5) 
+                //////    {
+                //////        Console.WriteLine(array[i]);
+                //////    }
+                //////}
+
+
+
+           }
+        }
+    }
+  }
+
+      
